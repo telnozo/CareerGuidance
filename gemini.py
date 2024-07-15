514e39111caeba0bd20ea2b070676001d0335e18
telnozo <welfare.devs@gmail.com>
2024-07-15 21:44:12 +0530

    Update gemini.py
    env var access using streamlit secrets

--- gemini.py
+++ gemini.py
@@ -1,8 +1,9 @@
 import os
+import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 # Creating the model
 generation_config = {
